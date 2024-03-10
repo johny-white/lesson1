a9c4f6d2696a7798439802f6ef6dac051b8bfd9d
white <white-johny@mail.ru>
2024-03-10 21:51:57 +0700

    lesson1:task4

---
+++ OneDrive/Рабочий стол/lesson1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 100;
+int b = 35;
+int c = 60;
+int d = 50;
+int e = 25;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
