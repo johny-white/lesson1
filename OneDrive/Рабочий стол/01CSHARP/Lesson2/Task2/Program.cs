582b65cf75226228335e1f6ad129696d66fdf160
white <white-johny@mail.ru>
2024-03-18 21:20:52 +0700

    add Task2

---
+++ OneDrive/Рабочий стол/01CSHARP/Lesson2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Создать массив и заполнить его целыми числами от 1 до N 
+(N - произвольное натуральное число). Вывести элементы массива на экран.
+*/
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+while(i < arr.Length)
+{
+    arr[i] = i + 1;
+    //Console.Write(arr[i]);
+    //Console.Write(" ");
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
